158e016c2d71a7e81659e83d017ef65daafc53f2
Konstantin <timokhin_74@hotmail.com>
2020-12-08 18:23:12 +0500

    Updates

---
+++ precache-manifest.fb58df9f8f8a4283b001210ba00b7c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a452a919db7137dc02ef6c70743b8bd1",
+    "url": "/my-todolist/index.html"
+  },
+  {
+    "revision": "2a029b66b7aee86dea3b",
+    "url": "/my-todolist/static/css/main.84802909.chunk.css"
+  },
+  {
+    "revision": "1eec52afb2268a58f61c",
+    "url": "/my-todolist/static/js/2.53774408.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-todolist/static/js/2.53774408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a029b66b7aee86dea3b",
+    "url": "/my-todolist/static/js/main.6fa63022.chunk.js"
+  },
+  {
+    "revision": "7b4c029b6dbd6cf0268e",
+    "url": "/my-todolist/static/js/runtime-main.92e6d4d2.js"
+  }
+]);
